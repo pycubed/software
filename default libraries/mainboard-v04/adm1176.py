2f87e40d932382b65b75d7a4fd2edcf46b92d00d
Max Holliday <maholli@stanford.edu>
2020-01-30 21:03:58 -0800

    adding hacky trick to reset board power via ADM1176. puts board into safemode

--- default libraries/mainboard-v04/adm1176.py
+++ default libraries/mainboard-v04/adm1176.py
@@ -32,7 +32,9 @@ class ADM1176:
 
         self.config('V_CONT,I_CONT')
     _cmd=bytearray(1)
-    _cmd[0] = 0b0000000
+    _cmd[0] = 0x00
+    _extcmd=bytearray(2)
+    _extcmd[0] = 0x83
 
     _BUFFER = bytearray(3)
     _STATUS = bytearray(1)
@@ -48,7 +50,7 @@ class ADM1176:
         if 'VRANGE' in value:
             self._cmd[0] |= (1<<4)
         with self.i2c_device as i2c:
-            i2c.write(self._cmd, end=1, stop=True)
+            i2c.write(self._cmd)
 
     def read(self):
         with self.i2c_device as i2c:
@@ -59,4 +61,15 @@ class ADM1176:
         _current = ((0.10584/4096)*raw_current)/self.sense_resistor # amperes
         return (_voltage,_current)
 
+    @property
+    def OFF(self):
+        self._extcmd[1]=1
+        with self.i2c_device as i2c:
+            i2c.write(self._extcmd)
 
+    @property
+    def ON(self):
+        self._extcmd[1]=0
+        with self.i2c_device as i2c:
+            i2c.write(self._extcmd)
+        self.config('V_CONT,I_CONT')
